0c94e6c9b5eeb5fe1af4c91ea9743dfe2aea1adc
pong7772 <visothipong7772@gmail.com>
2022-07-21 14:20:10 +0700

    change some ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { useLayoutEffect } from 'react';
+import { userIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/solid"
 const HomeScreen = () => {
     const navigation = useNavigation();
     useLayoutEffect(() => {
@@ -10,16 +11,20 @@ const HomeScreen = () => {
     }, [])
     return (
         <SafeAreaView>
-            <View className="flex-row items-centered mx-4 space-x-2 ">
+            <View className="flex-row items-centered mx-4 space-x-2 pb-3 ">
                 <Image source={require('../assets/logo.png')}
-                    className="h-9 mt-6 w-9 bg-gray-300 rounded-full p-2"
+                    className="h-9 mt-9 w-9 bg-gray-300 rounded-full "
                 />
-                <View className="mt-6">
-                    <Text className="text-gray-1000 font-bold size-medium">Delivery Now!</Text>
-                    <Text>Current Location</Text>
+                <View className="mt-9 flex-column items-left" >
+                    <Text className="text-gray-500 font-bold text-xs">Delivery Now!</Text>
+                    <View className="flex-row ">
+                        <Text className="text-pink-500 font-bold text-sm items-start" >Current Location
+                        </Text>
+                        <ChevronDownIcon size={24} color="#ff69b4" />
+                    </View>
                 </View>
-            </View>
-        </SafeAreaView>
+            </View >
+        </SafeAreaView >
     );
 }
 export default HomeScreen;
